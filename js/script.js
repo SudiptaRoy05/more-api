59e25505f0f7a0db238541c64ae7b69e3b671378
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-10-02 22:25:16 +0600

    static video card added

--- js/script.js
+++ js/script.js
@@ -4,12 +4,30 @@ const loadCategories = async () => {
         const res = await fetch(url);
         const data = await res.json();
         displayCategory(data.categories);
-        
-    }catch(error){
+
+    } catch (error) {
         console.log(error);
     }
 }
-const displayCategory = async(data) =>{
+
+const loadVideos = async () => {
+    try {
+        const url = 'https://openapi.programming-hero.com/api/phero-tube/videos';
+        const res = await fetch(url);
+        const data = await res.json();
+        displayVideos(data.videos);
+        // console.log(data.videos)
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+
+
+const displayCategory = async (data) => {
     const categoriBtn = document.getElementById('category');
 
     data.forEach(item => {
@@ -20,6 +38,34 @@ const displayCategory = async(data) =>{
     })
 }
 
+const displayVideos = async (data) => {
+    const vdoSec = document.getElementById('vdoSec');
+
+    data.forEach(item => {
+        const card = document.createElement('div');
+        card.classList.add = 'card card-compact'
+        card.innerHTML = `
+        <figure>
+            <img
+      src="${item.thumbnail}"
+      alt="Shoes" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        `
+        vdoSec.appendChild(card);
+    })
+}
+
+
+
+
 
 
 loadCategories();
+loadVideos();
