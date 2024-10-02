f703f7b60f2be76ad4d049e032d3ddf0f3584322
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-10-02 22:04:44 +0600

    category btn added dynamicly

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log('first')
+const loadCategories = async () => {
+    try {
+        const url = 'https://openapi.programming-hero.com/api/phero-tube/categories';
+        const res = await fetch(url);
+        const data = await res.json();
+        displayCategory(data.categories);
+        
+    }catch(error){
+        console.log(error);
+    }
+}
+const displayCategory = async(data) =>{
+    const categoriBtn = document.getElementById('category');
+
+    data.forEach(item => {
+        const btn = document.createElement('button');
+        btn.classList.add('btn');
+        btn.innerText = item.category;
+        categoriBtn.appendChild(btn)
+    })
+}
+
+
+
+loadCategories();
